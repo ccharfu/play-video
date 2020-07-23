5c0e90179d20688cded5c133a273473820e2398c
ccharfu <carlosuefagh@gmail.com>
2020-07-23 17:40:33 -0500

    Backend con webpack, babel y node

---
+++ README.md
@@ -0,0 +1,3 @@
+# Aprendiendo-React
+
+Este repositorio contiene un curso que se aprendio en platzi, en curso practico con react
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                use: ['babel-loader'],
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: './index.html'
+        })
+    ]
+}
